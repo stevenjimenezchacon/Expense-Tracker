0b83f245afb6f65deecf2ee31b5fe50451eeb5ef
stevenjimenezchacon <stevenjimenezchacon1@gmail.com>
2026-03-23 20:07:25 -0600

    agrega modelo User

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando 🚀");
+  res.send("API funcionando");
 });
 
-app.listen(5000, () => {
-  console.log("Servidor en puerto 5000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
